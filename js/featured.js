9966cc49ae0405c6cc4d6d56d8be0c53e8b1b3b3
LAURA CHAPMAN <lchapman@college.harvard.edu>
2021-12-12 13:55:49 -0500

    change featured pictures

--- js/featured.js
+++ js/featured.js
@@ -1,4 +1,4 @@
-generateContent("featured", "featured", 14);
+generateContent("featured", "featured", 15);
 
     var med_elems = document.getElementsByClassName("med-pics");
     for (var i=0; i<med_elems.length; i++){
